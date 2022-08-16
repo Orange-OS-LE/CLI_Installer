559ea590f00820dac2b31e31c1058ca1ceaf47d1
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 16:59:05 +0100

    .

--- script.py
+++ script.py
@@ -2,6 +2,12 @@ import archinstall
 
 print("Welcome to the Orange OS LE Installer")
 print("This will ask you some questions to help make your installation great!")
-print("1. What language do you use? ")
-test = archinstall.list_keyboard_languages()
+invalidInput = True
+while invalidInput:
+    choice = input("1. What language do you use? ")
+    try:
+        test = archinstall.select_language(choice)
+    except:
+        continue
+    invalidInput = False
 print(test)
