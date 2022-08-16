2c8f07a64839599e8649c26c7df9ae44abf4fb6b
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 17:24:25 +0100

    .

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ if not os.environ.get("SUDO_UID") and os.geteuid() != 0:
 
 print("Welcome to the Orange OS LE Installer")
 print("This will ask you some questions to help make your installation great!")
+input("Press enter when you are ready to start!")
 '''
 invalidInput = True
 while invalidInput:
@@ -37,4 +38,5 @@ print(out)
 menu = archinstall.GlobalMenu(data_store=archinstall.arguments)
 
 menu.enable("archinstall-language")
+menu.enable("abort")
 menu.run()
