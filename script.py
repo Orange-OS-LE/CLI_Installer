d368245357a7efeb22d4fac12a6612a41082372b
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 21:00:12 +0100

    .

---
+++ script.py
@@ -0,0 +1,19 @@
+print("Welcome to the Orange OS LE 1.0.0-alpha install script.")
+print("We will ask you a few questions to make sure you get a great configuration.")
+
+hard_drive = input("Please input your disk drive file, like /dev/sda")
+host_name = input(
+    "Please enter the name you want to give your computer (a.k.a your hostname)"
+)
+
+user_config = open("user_configuration.json", "w")
+user_config.write(
+    f"""
+"config_version": "2.5.0",
+"debug": false,
+"harddrives": [
+    "{hard_drive}"
+],
+"hostname": "{host_name}"
+"""
+)
