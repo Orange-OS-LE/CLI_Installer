e328717b814947b0d8bdcabd1ef8be121c64a2af
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 16:54:08 +0100

    .

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
+import archinstall
+
 print("Welcome to the Orange OS LE Installer")
+print("This will ask you some questions to help make your installation great!")
+print("1. What language do you use? ")
+test = archinstall.list_keyboard_languages()
+print(test)
