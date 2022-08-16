e2a77f037a8f7dea613b1c153f5225b8f8738fed
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 17:02:43 +0100

    .

--- script.py
+++ script.py
@@ -6,8 +6,26 @@ invalidInput = True
 while invalidInput:
     choice = input("1. What language do you use? ")
     try:
-        out = archinstall.select_language(choice)
+        out = archinstall.select_language(choice.capitalize())
     except:
+        print("Sorry, that didn't work. Here are all the options: ")
+        print(
+            """
+Czech
+Dutch
+English
+French
+German
+Italian
+Polish
+Portuguese
+Russian
+Spanish
+Swedish
+Turkish
+Urdu
+        """
+        )
         continue
     invalidInput = False
 print(out)
