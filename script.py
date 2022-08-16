b983032e904347b2abe7f827f5266bc9812d095a
Michael Halpin <halpinm2010@gmail.com>
2022-08-16 17:28:00 +0100

    Changed print to archinstall.log

--- script.py
+++ script.py
@@ -3,11 +3,15 @@ import archinstall, os
 if not os.environ.get("SUDO_UID") and os.geteuid() != 0:
     raise PermissionError("You need to run this script with sudo or as root.")
 
-print("Welcome to the Orange OS LE Installer")
-print("This will ask you some questions to help make your installation great!")
+archinstall.log("Welcome to the Orange OS LE Installer")
+archinstall.log(
+    "This will ask you some questions to help make your installation great!"
+)
 input("Press enter when you are ready to start!")
 menu = archinstall.GlobalMenu(data_store=archinstall.arguments)
 
 menu.enable("archinstall-language")
+menu.enable("keyboard-layout")
+# menu.enable()
 menu.enable("abort")
 menu.run()
