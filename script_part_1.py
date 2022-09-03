e488511419d2cefee9097d016b0f569d87fdae65
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2022-09-03 15:31:37 +0100

    possible fix

--- script_part_1.py
+++ script_part_1.py
@@ -219,3 +219,6 @@ user_disks.close()
 os.system(
     "sudo archinstall --silent --config ./user_configuration.json --creds ./user_credentials.json --disk_layouts ./user_disk_layout.json"
 )
+os.system(
+    "sudo archinstall --silent --config ./user_configuration.json --creds ./user_credentials.json --disk_layouts ./user_disk_layout.json"
+)
