51376cda749b619da47b6a7d805af57c99275aad
michaeleldar <66514999+michaeleldar@users.noreply.github.com>
2022-09-09 15:46:56 +0100

    turn off auto reboot

--- script_part_1.py
+++ script_part_1.py
@@ -237,4 +237,5 @@ else:
         "sudo archinstall --silent --config ./user_configuration.json --creds ./user_credentials.json --disk_layouts ./user_disk_layout.json"
     )
 
-os.system("reboot -h now")
+#os.system("reboot -h now")
+# removed for debugging
